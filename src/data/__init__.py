f9034ba3770064f473ba28f9b3caecef71627fd7
Flyzin <71525505+Flyzin@users.noreply.github.com>
2025-11-20 14:51:31 -0300

    chore: initial project setup (.gitignore, requirements and data folders)

---
+++ src/data/__init__.py
@@ -0,0 +1,9 @@
+from .load import load_raw_data, load_processed_data
+from .preprocess import basic_cleaning, save_processed
+
+__all__ = [
+    "load_raw_data",
+    "load_processed_data",
+    "basic_cleaning",
+    "save_processed",
+]
